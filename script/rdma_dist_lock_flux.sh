9e0b50ca8467a98e4e9edf531871c711cb1b8b04
Dong Young Yoon <dyoon@umich.edu>
2017-10-12 16:43:52 -0400

    updated scripts.

--- script/rdma_dist_lock_flux.sh
+++ script/rdma_dist_lock_flux.sh
@@ -1,56 +1,54 @@
 #!/bin/bash
 
-job_name="rdma_dist_lock"
+job_name="rdma_dist2"
 work_dir="/home/dyoon/temp/$job_name/dir"
 
-for num_node in 4
+# for lock_mode in simple_retry d2lm_2 drtm ncosed traditional
+for num_node in 16 
 do
   for num_user in 1
   do
-    for workload in simple hotspot
+    for workload in hotspot
     do
-      num_core=`expr 1 + $num_user`
-      resource_str="nodes=$num_node:ppn=$num_core,pmem=2gb,walltime=00:15:00"
+      #num_core=`expr 1 + $num_user`
+      resource_str="qos=flux,procs=$num_node,tpn=1,pmem=1gb,walltime=00:15:00"
       for duration in 600
       do
-        wall_time=`expr 5 + $duration / 60`
+        #wall_time=`expr 5 + $duration / 60`
         for num_lock_object in 1 10 100 1000
         do
           for request_per_tx in 1
           do
-            for lock_mode in remote-poll remote-queue
+            for think_time_type in simple
             do
-              for think_time_type in normal
+              for think_time_duration in 0 10 100 
               do
-                for se_rule in fail
+                for num_retry in 10 100 1000 10000 100000
                 do
-                  for es_rule in fail
+                  for random_backoff in true false
                   do
-                    for ee_rule in fail
+                    for lock_mode in simple_retry d2lm_2 drtm traditional
                     do
-                      for num_retry in 10000
+                      let "job_counter++"
+                      mkdir -p $work_dir$job_counter
+                      qsub -A mozafari_flux \
+                        -l "$resource_str" -N $job_name \
+                        -q flux \
+                        -e /home/dyoon/work/pbs/rdma/rdma-dist-lock-$lock_mode-$workload-$think_time_type-tt$think_time_duration-nn$num_node-user$num_user-obj$num_lock_object-req$request_per_tx-retry$num_retry-rb_$random_backoff-${duration}s.e \
+                        -o /home/dyoon/work/pbs/rdma/rdma-dist-lock-$lock_mode-$workload-$think_time_type-tt$think_time_duration-nn$num_node-user$num_user-obj$num_lock_object-req$request_per_tx-retry$num_retry-rb_$random_backoff-${duration}s.o \
+                        -v num_node=$num_node,work_dir=$work_dir,job_counter=$job_counter,duration=$duration,num_user=$num_user,num_lock_object=$num_lock_object,lock_mode=$lock_mode,se_rule=$se_rule,es_rule=$es_rule,ee_rule=$ee_rule,num_retry=$num_retry,request_per_tx=$request_per_tx,workload=$workload,think_time_type=$think_time_type,random_backoff=$random_backoff,think_time_duration=$think_time_duration \
+                        rdma_dist_lock.pbs
+                      sleep 10
+                      num_waiting_jobs=`qstat -i -u dyoon 2>&1 | grep $job_name | wc -l`
+                      num_running_jobs=`qstat -r -u dyoon 2>&1 | grep $job_name | wc -l`
+                      num_jobs=$(($num_waiting_jobs+$num_running_jobs))
+                      while [ $num_jobs -gt 0 ]
                       do
-                        let "job_counter++"
-                        mkdir -p $work_dir$job_counter
-                        qsub -A engin_flux \
-                          -l "$resource_str" -N $job_name \
-                          -q flux \
-                          -e /home/dyoon/work/pbs/rdma/rdma-dist-lock-$lock_mode-$workload-$think_time_type-nn$num_node-user$num_user-obj$num_lock_object-req$request_per_tx-retry$num_retry-$se_rule-$es_rule-$ee_rule-${duration}s.e \
-                          -o /home/dyoon/work/pbs/rdma/rdma-dist-lock-$lock_mode-$workload-$think_time_type-nn$num_node-user$num_user-obj$num_lock_object-req$request_per_tx-retry$num_retry-$se_rule-$es_rule-$ee_rule-${duration}s.o \
-                          -v num_node=$num_node,work_dir=$work_dir,job_counter=$job_counter,duration=$duration,num_user=$num_user,num_lock_object=$num_lock_object,lock_mode=$lock_mode,se_rule=$se_rule,es_rule=$es_rule,ee_rule=$ee_rule,num_retry=$num_retry,request_per_tx=$request_per_tx,workload=$workload,think_time_type=$think_time_type \
-                          rdma_dist_lock.pbs
-                        sleep 10
+                        sleep 1
                         num_waiting_jobs=`qstat -i -u dyoon 2>&1 | grep $job_name | wc -l`
                         num_running_jobs=`qstat -r -u dyoon 2>&1 | grep $job_name | wc -l`
                         num_jobs=$(($num_waiting_jobs+$num_running_jobs))
-                        while [ $num_jobs -gt 0 ]
-                        do
-                          sleep 1
-                          num_waiting_jobs=`qstat -i -u dyoon 2>&1 | grep $job_name | wc -l`
-                          num_running_jobs=`qstat -r -u dyoon 2>&1 | grep $job_name | wc -l`
-                          num_jobs=$(($num_waiting_jobs+$num_running_jobs))
-                          wait_time=$((wait_time+1))
-                        done
+                        wait_time=$((wait_time+1))
                       done
                     done
                   done
